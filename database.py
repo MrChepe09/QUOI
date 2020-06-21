9c5733fdfa90f0d45ac56346134790a042fd6b90
Bhupinder Singh <khanujabhupinder09@gmail.com>
2020-06-21 17:22:11 +0530

    Changed code to work with Postgres

--- database.py
+++ database.py
@@ -1,14 +1,37 @@
 from flask import g
 import sqlite3
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+import psycopg2
+from psycopg2.extras import DictCursor
 
 def connect_db():
-    sql = sqlite3.connect(os.path.join(basedir, 'questions.db'))
-    sql.row_factory = sqlite3.Row
-    return sql
+    conn = psycopg2.connect('postgres://ktrayavnmlusmc:558695612d4b89acd6cc4fb20c65271d617a9afa500df62a2ac438df35e19abb@ec2-34-230-231-71.compute-1.amazonaws.com:5432/daokuoflpjvhai', cursor_factory=DictCursor)
+    conn.autocommit = True
+    sql = conn.cursor()
+    return conn, sql
 
 def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    db = connect_db()
+    
+    if not hasattr(g, 'postgres_db_conn'):
+        g.postgres_db_conn = db[0]
+        
+    if not hasattr(g, 'postgres_db_cur'):
+        g.postgres_db_cur = db[1]
+        
+    return g.postgres_db_cur
+
+def init_db():
+    db = connect_db()
+    
+    db[1].execute(open('schema.sql', 'r').read())
+    db[1].close()
+    
+    db[0].close()
+    
+def init_admin():
+    db = connect_db()
+    
+    db[1].execute('update users set admin = True where name = %s', ('admin', ))
+    
+    db[1].close()
+    db[0].close()
